d65adb4e3fd3a64513a7bc82f565ca26840c7d76
Heqing Huang <feipenghhq@gmail.com>
2025-07-12 22:47:53 -0700

    Implement the vjtag_host to DE2 FPGA board
    - Need to use the actual FPGA board to test thd design and develop the
      program to interact with the FPGA

---
+++ fpga/de2/pin.tcl
@@ -0,0 +1,45 @@
+# KEY
+set_location_assignment PIN_G26 -to KEY
+
+# CLOCK
+set_location_assignment PIN_N2 -to CLOCK_50
+
+# LEDR
+set_location_assignment PIN_AE23 -to LEDR[0]
+set_location_assignment PIN_AF23 -to LEDR[1]
+set_location_assignment PIN_AB21 -to LEDR[2]
+set_location_assignment PIN_AC22 -to LEDR[3]
+set_location_assignment PIN_AD22 -to LEDR[4]
+set_location_assignment PIN_AD23 -to LEDR[5]
+set_location_assignment PIN_AD21 -to LEDR[6]
+set_location_assignment PIN_AC21 -to LEDR[7]
+set_location_assignment PIN_AA14 -to LEDR[8]
+set_location_assignment PIN_Y13  -to LEDR[9]
+set_location_assignment PIN_AA13 -to LEDR[10]
+set_location_assignment PIN_AC14 -to LEDR[11]
+set_location_assignment PIN_AD15 -to LEDR[12]
+set_location_assignment PIN_AE15 -to LEDR[13]
+set_location_assignment PIN_AF13 -to LEDR[14]
+set_location_assignment PIN_AE13 -to LEDR[15]
+
+
+# SW
+set_location_assignment PIN_N25 -to SW[0]
+set_location_assignment PIN_N26 -to SW[1]
+set_location_assignment PIN_P25 -to SW[2]
+set_location_assignment PIN_AE14 -to SW[3]
+set_location_assignment PIN_AF14 -to SW[4]
+set_location_assignment PIN_AD13 -to SW[5]
+set_location_assignment PIN_AC13 -to SW[6]
+set_location_assignment PIN_C13 -to SW[7]
+set_location_assignment PIN_B13 -to SW[8]
+set_location_assignment PIN_A13 -to SW[9]
+set_location_assignment PIN_N1 -to SW[10]
+set_location_assignment PIN_P1 -to SW[11]
+set_location_assignment PIN_P2 -to SW[12]
+set_location_assignment PIN_T7 -to SW[13]
+set_location_assignment PIN_U3 -to SW[14]
+set_location_assignment PIN_U4 -to SW[15]
+
+
+
---
+++ fpga/de2/top.sv
@@ -0,0 +1,70 @@
+// -------------------------------------------------------------------
+// Copyright 2025 by Heqing Huang (feipenghhq@gamil.com)
+// -------------------------------------------------------------------
+//
+// Project: UART Controller
+// Author: Heqing Huang
+// Date Created: 06/25/2025
+//
+// -------------------------------------------------------------------
+// Top level for the Arty FPGA board
+// Loopback the RX to TX
+// -------------------------------------------------------------------
+
+module top
+(
+    input         CLOCK_50,    // 50 MHz
+    input         KEY,         // Used as RESET, low active
+
+    output [15:0] LEDR,
+    input  [15:0] SW
+);
+
+    logic        rst_n_out;
+    logic [15:0] address;
+    logic        wvalid;
+    logic [15:0] wdata;
+    logic        wready;
+    logic        rvalid;
+    logic        rready;
+    logic        rrvalid;
+    logic [15:0] rdata;
+
+    logic [15:0] switch;
+    logic [15:0] led;
+
+    always @(posedge CLOCK_50) begin
+        if (!KEY) begin
+            switch <= 0;
+            led    <= 16'hFFFF;
+            rrvalid <= 1'b0;
+        end
+        else begin
+            switch <= SW;
+            //if (!rst_n_out) led <= 0;
+            if (wvalid && address == 4) led <= wdata;
+            rrvalid <= rvalid;
+        end
+    end
+
+    assign LEDR = led;
+
+    assign rready = 1;
+    assign wready = 1;
+
+    vjtag_host #(.AW(16), .DW(16))
+    u_host (
+        .clk       (CLOCK_50),
+        .rst_n     (KEY),
+        .rst_n_out (rst_n_out),
+        .address   (address),
+        .wvalid    (wvalid),
+        .wdata     (wdata),
+        .wready    (wready),
+        .rvalid    (rvalid),
+        .rready    (rready),
+        .rrvalid   (rrvalid),
+        .rdata     (switch)
+    );
+
+endmodule
